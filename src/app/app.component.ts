6a6aa4e02f56c41320401ebf17f99bb4eb9268da
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-06-19 20:36:50 +0200

    Setup of project and creating first component with a new title property.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+/* The component class code, written in TypeScript. */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /* title property */
+  title = 'Tour of Heroes';
+}
