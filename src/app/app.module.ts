478014c5d3307303d8f75563c1498b90a454b3e3
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-06-21 11:19:05 +0200

    Input und Liste in heroescomponent hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+/* FormsModule damit in heroescomponent template der input mit ngModel funktioniert */
+
+/* Angular needs to know how the pieces of your application fit together and what other files 
+and libraries the application requires. This information is called metadata.
+Some of the metadata is in the @Component decorators that you added to your component classes. 
+Other critical metadata is in @NgModule decorators.
+The most important @NgModule decorator annotates the top-level AppModule class.
+The Angular CLI generated an AppModule class in src/app/app.module.ts when it created the project. 
+This is where you opt-in to the FormsModule.
+*/
+
+/* Every component must be declared in exactly one NgModule. --> CLI did that for me: HeroesComponent */
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +24,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes"; // imports mock-heroes
 
 /* @Component ist eine Decoratorfunktion, in der Metadaten der Komponente angegeben werden */
 /*  selector: CSS-Selektor.
@@ -17,16 +18,27 @@ import { Hero } from "../hero";
 export class HeroesComponent implements OnInit {
   // hero property of type object by adding imported Hero interface
   // Initialisierung mit einer Id von 1 und dem Namen 'Windstorm'
-  hero: Hero = {
+  /*hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  };*/
+
+
+  // In the same file (HeroesComponent class), define a component property called 
+  // heroes to expose the HEROES array for binding.
+  heroes = HEROES;
+  /* onSelect() method, which assigns the clicked 
+  hero from the template to the component's selectedHero.*/
+  selectedHero?: Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 /* ngOnInit() ist eine Lifecycle-Hook, die Angular kurz nach Komponentenerstellung ruft. Darin sollte Initialisierungslogik platziert werden */
