e07a0b84f0a7095c5e9e0134f51ab5a3a204e2bd
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-06-21 11:49:05 +0200

    Neue Struktur

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 /* HeroesComponent class file */
-
+/*
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes"; // imports mock-heroes
@@ -9,13 +9,13 @@ import { HEROES } from "../mock-heroes"; // imports mock-heroes
     templateUrl: Ort von der Komponenten Template-Datei
     styleUrls: Ort von den privaten CSS-Styles der Komponente
 */
-
+/*
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
-})
-export class HeroesComponent implements OnInit {
+})*/
+//export class HeroesComponent implements OnInit {
   // hero property of type object by adding imported Hero interface
   // Initialisierung mit einer Id von 1 und dem Namen 'Windstorm'
   /*hero: Hero = {
@@ -26,10 +26,12 @@ export class HeroesComponent implements OnInit {
 
   // In the same file (HeroesComponent class), define a component property called 
   // heroes to expose the HEROES array for binding.
-  heroes = HEROES;
+ // hero?: Hero;
+
+ // heroes = HEROES;
   /* onSelect() method, which assigns the clicked 
   hero from the template to the component's selectedHero.*/
-  selectedHero?: Hero;
+  /*selectedHero?: Hero;
 
   constructor() { }
 
@@ -40,6 +42,30 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 }
-
+*/
 /* ngOnInit() ist eine Lifecycle-Hook, die Angular kurz nach Komponentenerstellung ruft. Darin sollte Initialisierungslogik platziert werden */
 /* Die Komponentenklasse wird exportiert, damit sie irgendwoanders importiert werden kann */
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+  @Input() hero?: Hero;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
